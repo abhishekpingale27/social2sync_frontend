fb52d594c1130801dbafc4ef5020fd2e58cbc79f
abhishekpingale27 <124373173+abhishekpingale27@users.noreply.github.com>
2025-05-26 13:06:07 +0530

    Update ChatSupport.jsx

--- src/pages/ChatSupport.jsx
+++ src/pages/ChatSupport.jsx
@@ -97,7 +97,7 @@ const MainContent = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/chatbot/", {
+      const response = await fetch("https://social2sync.vercel.app/api/chatbot/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ query: messageText }),
